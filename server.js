3e11876cb8a62b98d378f9d9f73730d52a6dc11e
Anjali Shukla <anjishukla890@gmail.com>
2026-08-06 00:21:31 +0530

    fix video serving

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
+
+app.use("/videos", express.static(path.join(__dirname, "videos")));
 app.use(express.static(path.join(__dirname,"frontend")));
 
 app.get("/",(req,res)=>{
